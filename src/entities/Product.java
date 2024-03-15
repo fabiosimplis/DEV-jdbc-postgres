d4cd60bbb231629cce3b1b78c886080e833fba11
fabio.simplis <31253667+fabiosimplis@users.noreply.github.com>
2024-03-15 10:22:22 -0300

    Add entidades

---
+++ src/entities/Product.java
@@ -0,0 +1,61 @@
+package entities;
+
+public class Product {
+
+    private Long id;
+    private String name;
+    private Double price;
+    private String description;
+    private String img;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getImg() {
+        return img;
+    }
+
+    public void setImg(String img) {
+        this.img = img;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", price=" + price +
+                ", description='" + description + '\'' +
+                ", img='" + img + '\'' +
+                '}';
+    }
+}
